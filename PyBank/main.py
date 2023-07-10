8d81435b0ee1d76e1798444023e110df29a5c555
mxk105620 <monilakhadka@Monilas-Air.attlocal.net>
2023-07-10 14:15:17 -0500

    python

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,84 @@
 import os
-
-# Module for reading CSV files
 import csv
+import textwrap
 cwd=os.getcwd()
 csvpath = os.path.join(cwd,"Resources/budget_data.csv")
 
 Date =[]
 Profit_losses= []
+netchange= []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header= next(csvreader)
-    
-for row in csvreader:
+    for row in csvreader:
         Date.append(row[0])
-        Profit_losses.append(row[1])
-        
-        total_month= len(Date)
-        print(total_month)
-       
-        Total = sum(Profit_losses)
-        Print(Total)
+        Profit_losses.append(int(row[1]))
 
-        Greatest_decrease = min(Profit_losses)
-        for row in csvreader:
-            if Profit_losses==row[1]:
-                print(row,Greatest_decrease)
 
-        Greatest_increase = max(Profit_losses)  
-        for row in csvreader:
-            if Profit_losses== row[1]:
-                print(row, Greatest_increase)
+total_month= len(Date)
+c= ("Total Months: " + str(total_month))
+
+Sum= sum(Profit_losses)
+d= ("Total: "+str(Sum))
+
+
+for y in range(len(Profit_losses)):
+    if y<(total_month-1):
+       netchange.append(Profit_losses[y+1]-Profit_losses[y])
+
+netchangetotal=sum(netchange)
+Average_change=(float(netchangetotal)/(total_month-1))
+#e1=(Average_change, "type: ", type(Average_change))
+e=("Average Change: "+ "$"+str(Average_change))
+
+Greatest_increase = max(netchange)  
+
+for x in netchange:
+    if x == Greatest_increase:
+        index= netchange.index(x)
+        b= Date[index]
+
+f=("Greatest Increase in Profits: "+str(b)+ " "+ str(Greatest_increase))
+
+
+Greatest_decrease = min(netchange)
+for y in netchange:
+    if y == Greatest_decrease:
+        index= netchange.index(y)
+        d= Date[index]
+
+g=("Greatest Decrease in Profits: "+ str(d)+ " "+ str(Greatest_decrease))
+
+#summary=zip(c,d,e,f,g)
+#summary=zip(total_month,str(Sum), str(Average_change), str(Greatest_increase),b,str(Greatest_decrease),d)
+
+output_file=os.path.join(cwd,"analysis/budget_data_final.txt")
+#with open(output_file, 'w') as output_file:
+    # output_file.write("Financial analysis")
+    # output_file.write(c)
+    # output_file.write(d)
+    # output_file.write(e)
+    # output_file.write(f)
+    # output_file.write(g)
     
+output =(
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_month}\n"
+    f"Total: ${Sum}\n"
+    f"Average Change: ${Average_change:.2f}\n"
+    f"Greatest Increase in Profits: {b} (${Greatest_increase})\n"
+    f"Greatest Decrease in Profits: {d} (${Greatest_decrease})\n")
+
+print(output)
+# Export the results to text file
+with open(output_file, "w") as txt_file:
+    txt_file.write(output)
+
+ 
+    
+   
+        
+        
+
--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,29 +1,67 @@
 import os
 import csv
-cwd=os.getcwd()
-csvpath = os.path.join(cwd,"Resources/election_data.csv")
 
-def student (voter_data):
-    Voter_ID= int(voter_data[0])
-    County= str(voter_data[1])
-    Candidate= str(voter_data[2])
+csvpath = os.path.join("Resources/election_data.csv")
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    total_voters=len(Voter_ID)
-    print(total_voters)
+Voter_ID= []
+County =[]
+Candidate_options=[]
 
-
-
+candidate_votes={}
+wining_count=0
+total_votes=0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-set_res=set(Candidate)
-list_res= (list(set_res))
-   
- for item in list_res:
-        print(item)
-   # for row in csvreader:
-    #    print(row)
-   
+    header=next(csvreader)
+    for row in csvreader:
+        total_votes=total_votes+1
+        candidate_name=row[2]
+        
+        if candidate_name not in candidate_votes:
+            Candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=1
+        else:   
+            candidate_votes[candidate_name]=candidate_votes[candidate_name]+1
 
-#names of all candidates + percentage + total votes
-#winner name
+for candidate_name in candidate_votes:
+    votes= candidate_votes[candidate_name]
+    if votes>wining_count:
+        wining_count=votes
+        wining_candidate=candidate_name
+print("winner:", wining_count, wining_candidate)
+
+def percentage(candidate_votes, total_votes):
+    percentage= round(100*(candidate_votes/total_votes), 3)
+    return percentage
+    
+  
+output_file=os.path.join("analysis/election_data_final.txt")
+
+output =(
+    f"Election Results\n"
+    f"----------------------------\n"
+    f"Total Votes: {total_votes}\n"
+    f"----------------------------\n"
+)
+for name in candidate_votes:   
+    pct = percentage(candidate_votes[name], total_votes)
+    candidate_line= f"{name}: {pct}% ({candidate_votes[name]})\n"
+    output+=candidate_line
+
+output+=(
+    f"----------------------------\n"
+    f"Winner: {wining_candidate}\n"
+    f"----------------------------\n"
+)
+print(output)
+
+with open(output_file, "w") as txt_file:
+    txt_file.write(output)
+
+
+
+
+    
+
